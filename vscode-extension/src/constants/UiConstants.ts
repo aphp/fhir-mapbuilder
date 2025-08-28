213321fc1cfb63027bd97dc5004451f9cff2c797
David Ouagne <david.ouagne@aphp.fr>
2025-08-28 22:17:32 +0200

    (ui/ux): Improve titles of channels

--- vscode-extension/src/constants/UiConstants.ts
+++ vscode-extension/src/constants/UiConstants.ts
@@ -3,10 +3,10 @@ import * as vscode from 'vscode';
 export class UiConstants {
     public static readonly extensionPublisher = 'aphp.fhir-mapbuilder';
     public static readonly configName = 'FhirMapBuilder';
-    public static readonly principalChannelName = "FhirMapBuilder";
-    public static readonly detailsChannelName = "FhirMapBuilderValidation";
-    public static readonly principalChannel = vscode.window.createOutputChannel(UiConstants.principalChannelName);
-    public static readonly detailsChannel = vscode.window.createOutputChannel(UiConstants.detailsChannelName);
+    public static readonly principalChannelTitle = "FHIR MapBuilder";
+    public static readonly detailsChannelTitle = "FHIR MapBuilder Service";
+    public static readonly principalChannel = vscode.window.createOutputChannel(UiConstants.principalChannelTitle);
+    public static readonly detailsChannel = vscode.window.createOutputChannel(UiConstants.detailsChannelTitle);
     public static readonly fmlFilesPathToWatch = '**/*.fml';
     public static readonly qaPathToWatch = '**/output/qa.json';
 }
